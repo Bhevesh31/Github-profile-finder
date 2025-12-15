e3f8c37516d9187fd08f34d80d7eec4a03204f97
Bhevesh Sahu <sahubhevesh867@gmail.com>
2025-12-15 21:39:11 +0530

    update input field

--- src/components/search_component/SearchInput.jsx
+++ src/components/search_component/SearchInput.jsx
@@ -1,12 +1,15 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 const SearchInput = (props) => {
 
     const [inputName, setInputName] = useState("")
+
+    const inputRef = useRef(null)
+
     const formHandler=(e)=>{
         e.preventDefault();
         if(inputName===""){
-            alert("Enter a username")
+            inputRef.current.focus();
         }
         setInputName("");
 
@@ -23,9 +26,9 @@ const SearchInput = (props) => {
         <form onSubmit={(e)=>
             {formHandler(e)}
             } className='flex gap-4 flex-col sm:flex-row mt-2 sm:mt-0 justify-center items-center'>
-            <input value={inputName} onChange={(e)=>{
+            <input ref={inputRef} value={inputName} onChange={(e)=>{
                 setInputName(e.target.value);               
-            }} className='border text-slate-100 bg-[#115E59] sm:w-120 border-[#0F766E]  px-2 py-1 sm:px-2.5 rounded-xl sm:py-2.5 focus:outline-none focus:border-[#5EEAD4] placeholder-slate-300 ' type="text" placeholder=' Search username' />
+            }} className='border text-slate-100 bg-[#115E59] sm:w-120 border-[#0F766E]  px-2 py-1 sm:px-2.5 rounded-xl sm:py-2.5 focus:outline-3 focus:outline-[#5EEAD4]  placeholder-slate-300 ' type="text" placeholder=' Search username' />
 
             <button className='w-[40%] sm:w-[32%] px-1 sm:px-0 py-0.5  sm:py-2  text-sm sm:text-lg bg-teal-600 hover:bg-teal-500 text-white rounded-lg shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer hover:scale-105 active:scale-95  '>Search</button>
         </form>
